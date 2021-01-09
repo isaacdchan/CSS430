2d175a1650964d8174883a98413f3cac96e29914
Isaac Chan <isaac.d.chan@gmail.com>
2021-01-09 02:33:57 -0800

    refactored parent child processes into functions

--- P1/test.c
+++ P1/test.c
@@ -15,11 +15,27 @@ int parseArgs(char* rawInput, char** args) {
 	while(ptr != NULL) {
 		args[i] = ptr;
 		ptr = strtok_r(NULL, delim, &saveState);
-		
 		i++;
 	}
 
 	return i;
+}
+
+void checkRepeatCommand(char* command, char* prevCommand) {
+	if (strcmp(command, "!!\n") == 0) {
+		if (prevCommand == NULL) {
+			// throw error
+		} else {
+			*command = *prevCommand;
+			printf("Assigned command to: %s\n", prevCommand);
+			// *args = *prevArgs;
+		}
+	}
+}
+void checkExitCommand(char* command, int* shouldRun) {
+	if (strcmp(command, "exit\n") == 0) {
+		*shouldRun = 0;
+	}
 }
 
 void logArgs(char** args, int numArgs) {
@@ -29,11 +45,34 @@ void logArgs(char** args, int numArgs) {
 		printf("\tParam %d: %s\n", i, args[i]);
 	}
 }
+int checkAmpersand(char** args, int numArgs) {
+	int inBackground = (args[numArgs - 1] == "&"); // do not pass & into execvp
+}
+
+void handleChildProcess(char* command, char** args, int* shouldRun) {
+	printf("In child\n");
+
+	int rc = execvp(command, args);
+	shouldRun = 0;
+	printf("Finished child\n");
+}
+void handleParentProcess(int inBackground, int status, char* prevCommand, char * command) {
+	printf("In parent\n");
+	if (!inBackground) {
+		wait(&status);
+	}
+
+	strcpy(prevCommand, "test");
+	printf("Assigned prevCommand to: %s\n", command);
+	printf("Finished parent\n");
+	// *prevArgs = *args;
+}
 
 int main(void)
 {
   char* args[MAX_LINE/2 + 1]; // command line arguments
 	char* prevArgs[MAX_LINE/2 + 1];
+	char* command = NULL;
 	char* prevCommand = NULL;
 	char rawInput[256];
   int shouldRun = 1; // flag to determine when to exit program
@@ -46,50 +85,22 @@ int main(void)
     fflush(stdout);
 
 		int numArgs = parseArgs(rawInput, args);
-		char* command = args[0];
+		command = args[0];
 
-		logArgs(args, numArgs);
+		// logArgs(args, numArgs);
+		checkRepeatCommand(command, prevCommand);
+		checkExitCommand(command, &shouldRun);
+		int inBackground = checkAmpersand(args, numArgs);
 
-		if (strcmp(command, "exit\n") == 0) {
-    	printf("exiting!\n");
-			shouldRun = 0;
-		}
-		if (strcmp(command, "!!\n") == 0) {
-			if (prevCommand == NULL) {
-				// throw error
-			} else {
-				*command = *prevCommand;
-				*args = *prevArgs;
-			}
-		}
-
-		int inBackground = (args[numArgs - 1] == "&"); // do not pass & into execvp
 		int pid = fork();
 
-		if (pid == 0) { // child process
-			int rc = execvp(command, args);
-			printf("%s\n", command);
-			// int rc = execlp("/bin/ls", "ls", "-l", (char *) 0);
-			shouldRun = 0;
+		if (pid == 0) {
+			handleChildProcess(command, args, &shouldRun);
+		}	else {
+			handleParentProcess(inBackground, status, prevCommand, command);
 		}
-		else { // parent process
-			if (!inBackground) {
-				wait(&status);
-			}
+		// printf("pid: %d, shouldRun: %d\n", pid, shouldRun);
 
-			*prevCommand = *command;
-			*prevArgs = *args;
-		}
-
-		printf("pid: %d, shouldRun: %d\n", pid, shouldRun);
-
-
-    /**
-    * After reading user input, the steps are:
-    * (1) fork a child process using fork()
-    * (2) the child process will invoke execvp()
-    * (3) parent will invoke wait() unless command included &
-    */
   }
 
   return 0;
